bb4b9f41615d5e5b2cd09f6eeab6f1c4554d5709
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-14 15:17:32 +0800

    feat:react-router

--- src/containers/Detail/index.js
+++ src/containers/Detail/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 class Detail extends React.Component {
 	render() {
-		return <p>Detail，url参数：{this.props.params.id}</p>;
+		let { match } = this.props;
+		return <p>Detail，url参数：{match.params.id}</p>;
 	}
 }
 
-export default Detail;
+export default withRouter(Detail);
--- src/containers/List/index.js
+++ src/containers/List/index.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 class List extends Component {
+	constructor(props) {
+		super();
+	}
+	gotoDetail(item) {
+		this.props.history.push(`/detail/${item}`);
+	}
 	render() {
 		const arr = [ 1, 2, 3 ];
 		return (
 			<ul>
 				{arr.map((item, index) => {
-					return <li key={index}>js jump to {item}</li>;
+					return (
+						<li key={index} onClick={this.gotoDetail.bind(this, item)}>
+							js jump to {item}
+						</li>
+					);
 				})}
 			</ul>
 		);
 	}
 }
 
-export default List;
+export default withRouter(List);
